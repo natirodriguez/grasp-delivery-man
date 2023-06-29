74321336433dfdf7d1d60555d8d1703007a4ea80
natirodriguez <nati.stefania@gmail.com>
2023-06-29 10:34:40 -0300

    Update con nueva matriz

--- Main.py
+++ Main.py
@@ -9,20 +9,32 @@ grafoEjemplo = [[-1, 10, 15, 20, 8],
          [20, 25, 30, -1, 23],
          [8, 5, 14, 23,-1]]
 
+test_graph = [
+[0,   456, 605, 859, 328, 611, 289, 982, 286, 938],
+[456, 0,   302, 535, 553, 101, 801, 929, 605, 901],
+[605, 302, 0,   158, 919, 57,  687, 226, 949, 948],
+[859, 535, 158, 0,   261, 344, 757, 262, 744, 283],
+[328, 553, 919, 261, 0,   890, 431, 19,  793, 271],
+[611, 101, 57,  344, 890, 0,   18,  181, 832, 871],
+[289, 801, 687, 757, 431, 18,  0,   581, 71,  977],
+[982, 929, 226, 262, 19,  181, 581, 0,   107, 309],
+[286, 605, 949, 744, 793, 832, 71,  107, 0,   991],
+[938, 901, 948, 283, 271, 871, 977, 309, 991, 0]
+]
+
 #Se comenta el greedy random y el local search ya que internamente el grasp llama n veces a esas dos clases
 #Para un solo elemento se puede coentar el grasp para que no corra tanto y ver el resultado particular de ambos
 
-
-greedyRandom = GreedyRandom(grafoEjemplo)
+greedyRandom = GreedyRandom(test_graph, 30) #agarro este porcentaje para que pueda traer mas de un elem random
 caminoRandomizado, scoreCamino = greedyRandom.repartidorRandom()
 
-localSearch = LocalSearch(grafoEjemplo, caminoRandomizado, scoreCamino)
+localSearch = LocalSearch(test_graph, caminoRandomizado, scoreCamino)
 mejorResultado, mejorScore, solucionesRegistradas = localSearch.findBest()
 
 plotter = Plotter()
 plotter.solucion_plot(solucionesRegistradas)
 """
 
-grasp = Grasp(grafoEjemplo)
+grasp = Grasp(grafoEjemplo, 30)
 grasp.solucionGRASP()
 """
